0a84221304730b651875fb5bb837e001fb051aa5
Larisa <kosmetichka@mail.ru>
2017-03-02 18:41:31 +0300

    Терзание ума ни в чем не твердового

--- blocks/head.php
+++ blocks/head.php
@@ -19,11 +19,7 @@
                         </div>
                       </div>
                      
-      <div class="col-md-4 col-sm-4"> <div id="text-head-middle">Столешницы <br>из искусственного камня от производителя
-      <br></div>
-      <div id="mail">
-
-       <p><a href="mailto:info@prime-stone.ru" target="_blank"><img src="images/email.png">info@stoleshka.ru</a></p>
+      <div class="col-md-6 col-sm-4"> <div id="text-head-middle"><h1>Столешницы <br>из искусственного камня от производителя</h1>
       </div>
       </div>
        <div class="col-md-2 col-sm-2 col-xs-6">
@@ -34,66 +30,93 @@
    <h3>8-495-227-21-31<br></h3>
    </div>
    <div id="mob2"<h3>
-    8-499-184-27-37</h3>
+    8-499-184-37-27</h3>
     </div>
       </div>
-      <div class="col-md-2">
-         <div id="work">Время работы<br>
-        ПН-ПТ<br>
-         с 10.00 - до 18.00
-         <a href="#">
-         <br>
-         Посмотреть на карте</a>
-         
-      </div>
-      </div>
-       <div class="col-md-2 ">
-       
-          <div id="zamer">
-         <a href="#"> 
-          <img src="images/zamer.png">
-          <br>
+      <div class="col-md-2 col-sm-4"> 
+      <div id="mail">
 
-         Вызвать замерщика</a>
-        </div>
-         </div>
+       <p><a href="mailto:info@prime-stone.ru" target="_blank"><img src="images/email.png"><br>
+       info@stoleshka.ru</a></p>
+      </div>
+      </div>
+     
+       
          </div>
       </div>
                   </div>
-<nav role="navigation" class="navbar navbar-default">
-  <!-- Brand and toggle get grouped for better mobile display -->
+<!-- Меню navbar -->
+<nav class="navbar navbar-inverse">
+  <!-- Бренд и переключатель, который вызывает меню на мобильных устройствах -->
   <div class="navbar-header">
-    <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle navbar-left" >
+    <!-- Кнопка с полосочками, которая открывает меню на мобильных устройствах -->
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
       <span class="sr-only">Toggle navigation</span>
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a href="#" class="navbar-brand">Бренд</a>
+
+     <!-- Содержимое меню (коллекция навигационных ссылок, формы и др.) -->
+  <div class="collapse navbar-collapse" id="main-menu" >
+      <!-- Список ссылок, расположенных слева -->
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Главная</a></li>
-      </ul>
-  </div>
-  <!-- Collection of nav links, forms, and other content for toggling -->
-  <div id="navbarCollapse" class="collapse navbar-collapse">
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Главная</a></li>
-      <li><a href="#">Статьи</a></li>
+      <!--Элемент с классом active отображает ссылку подсвеченной -->
+      <li class="active"><a href="#">Столешка ру<span class="sr-only">(current)</span></a></li>
       <li class="dropdown">
-        <a data-toggle="dropdown" class="dropdown-toggle" href="#">Новости <b class="caret"></b></a>
-        <ul role="menu" class="dropdown-menu">
-          <li><a href="#">Windows</a></li>
-          <li><a href="#">Mac OS</a></li>
-          <li><a href="#">Linux</a></li>
-          <li class="divider"></li>
-          <li><a href="#">Другие системы</a></li>
-        </ul>
-      </li>
-    </ul>
-   
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#">Войти</a></li>
-    </ul>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Продукция <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Столешницы для кухни</a></li>
+            <li><a href="#">Столешницы для ванной комнаты</a></li>
+            <li><a href="#">Подоконники</a></li>
+            <li><a href="#">Барные стойки</a></li>
+            <li><a href="#">Ресепшн</a></li>
+            <li><a href="#">Столешницы для санузлов</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Мойки</a></li>
+            <li><a href="#">Раковины</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Изделия для лестницы</a></li>
+          </ul>
+             <li><a href="#">Акции</a></li>
+        <li><a href="#">Цены</a></li>
+        </li>
+       <li><a href="#">Наши работы</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Акриловый камень <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Samsung Staron</a></li>
+            <li><a href="#">LG Hi Macs</a></li>
+            <li><a href="#">Corian</a></li>
+            <li><a href="#">Hanex</a></li>
+            <li><a href="#">Tristone</a></li>
+            <li><a href="#">Grandex</a></li>
+            <li><a href="#">Kerrock</a></li>
+            <li><a href="#">Montelli</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Кварцевый камень <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Samsung Radianz</a></li>
+            <li><a href="#">Caesarstone</a></li>
+            <li><a href="#">Cambria</a></li>
+            <li><a href="#">Plazastone</a></li>
+            <li><a href="#">Scorino</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Еще<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Наше производство</a></li>
+            <li><a href="#">Кромки и бортики для столешниц</a></li>
+            <li><a href="#">Стеновые панели</a></li>
+            <li><a href="#">Накладки для ванных</a></li>
+            <li><a href="#">Тумбы для ванных комнат</a></li>
+          </ul>
+        </li>
+      </ul>
+     
   </div>
 </nav>
    
