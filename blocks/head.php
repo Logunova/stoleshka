d0ba75e76bd896d0139a32a6c2fa7a24de00cef8
Larisa <kosmetichka@mail.ru>
2017-03-02 13:53:30 +0300

    Столешка

---
+++ blocks/head.php
@@ -0,0 +1,102 @@
+<head>
+ <link rel="stoleshka.loc\favicon.ico">
+     <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css">
+     <link rel="stylesheet" href="vendor/jquery-ui/jquery-ui.css">
+     <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.css">
+     <link rel="stylesheet" href="vendor/fancybox/css/jquery.fancybox.css">
+     <link rel="stylesheet" href="/css/style.css" rel="stylesheet">
+     <script src="/vendor/jquery/jquery.js"></script>
+       <script src="/vendor/bootstrap/js/bootstrap.js"></script>
+             </head>
+<body>
+  <!-- Шапка-->
+     <div class="container-fluid">
+      <div id="headcont">
+              <div class="row">
+                     <div class="col-md-2 col-sm-3 col-xs-6">
+                       <div id="logo">
+                        <a href="index.php"><img src="images/logostoleshka.png"></a>
+                        </div>
+                      </div>
+                     
+      <div class="col-md-4 col-sm-4"> <div id="text-head-middle">Столешницы <br>из искусственного камня от производителя
+      <br></div>
+      <div id="mail">
+
+       <p><a href="mailto:info@prime-stone.ru" target="_blank"><img src="images/email.png">info@stoleshka.ru</a></p>
+      </div>
+      </div>
+       <div class="col-md-2 col-sm-2 col-xs-6">
+        <div id="text-tele">
+       <img src="images/tele.png">
+       </div>
+       <div id="mob"
+   <h3>8-495-227-21-31<br></h3>
+   </div>
+   <div id="mob2"<h3>
+    8-499-184-27-37</h3>
+    </div>
+      </div>
+      <div class="col-md-2">
+         <div id="work">Время работы<br>
+        ПН-ПТ<br>
+         с 10.00 - до 18.00
+         <a href="#">
+         <br>
+         Посмотреть на карте</a>
+         
+      </div>
+      </div>
+       <div class="col-md-2 ">
+       
+          <div id="zamer">
+         <a href="#"> 
+          <img src="images/zamer.png">
+          <br>
+
+         Вызвать замерщика</a>
+        </div>
+         </div>
+         </div>
+      </div>
+                  </div>
+<nav role="navigation" class="navbar navbar-default">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle navbar-left" >
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a href="#" class="navbar-brand">Бренд</a>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Главная</a></li>
+      </ul>
+  </div>
+  <!-- Collection of nav links, forms, and other content for toggling -->
+  <div id="navbarCollapse" class="collapse navbar-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Главная</a></li>
+      <li><a href="#">Статьи</a></li>
+      <li class="dropdown">
+        <a data-toggle="dropdown" class="dropdown-toggle" href="#">Новости <b class="caret"></b></a>
+        <ul role="menu" class="dropdown-menu">
+          <li><a href="#">Windows</a></li>
+          <li><a href="#">Mac OS</a></li>
+          <li><a href="#">Linux</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Другие системы</a></li>
+        </ul>
+      </li>
+    </ul>
+   
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Войти</a></li>
+    </ul>
+  </div>
+</nav>
+   
+               
+</body>
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include 'blocks/head.php';
+
+
+?>
